4911bbb11115c5f995894ebce7225a42e58ec0db
Crystal Martin <crystalmartin@Crystals-MacBook-Air.local>
2020-08-16 19:57:46 -0500

    built out useForm.js, problems defining showSuccessMessage

--- client/src/components/CheckoutForm.js
+++ client/src/components/CheckoutForm.js
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 
+import useForm from "./useForm";
+
+
+
 const initialValue = {
   firstName: "",
   lastName: "",
@@ -13,18 +17,21 @@ const initialValue = {
 // Build out the logic needed for a form custom hook (see the useForm.js file)
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
-const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+//const CheckoutForm = (props) => {
+ // const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+//const [values, setValues] = useState(initialValue);
 
-  const handleChanges = (e) => {
-    setValues({ ...values, [e.target.name]: e.target.value });
-  };
+  const CheckoutForm = () => {
+    const { values, handleChanges, handleSubmit } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
-  };
+  //const handleChanges = (e) => {
+   // setValues({ ...values, [e.target.name]: e.target.value });
+  //};
+
+  //const handleSubmit = (e) => {
+  //  e.preventDefault();
+  //  setShowSuccessMessage(true);
+  //};
 
   return (
     <>
--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,28 @@
 // write your custom hook here to control your checkout form
+
+import { useState } from 'react';
+
+const useForm = (callback) => {
+   
+    const [showSuccessMessage, setShowSuccessMessage]= useState(false);
+  const [values, setValues] = useState(initialValue);
+
+  const handleSubmit = (event) => {
+    if (event) event.preventDefault();
+    setShowSuccessMessage(true);
+      callback();
+  };
+
+  const handleChanges = (event) => {
+    setValues(values => ({ ...values, [event.target.name]: event.target.value }));
+  };
+
+  return {
+    handleChanges,
+    handleSubmit,
+    values,
+    
+  }
+};
+
+export default useForm;
