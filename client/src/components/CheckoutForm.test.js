7a2e33020b1fc119b2a20a2aff3e3d475641de8d
Crystal Martin <crystalmartin@Crystals-MacBook-Air.local>
2020-12-13 21:01:56 -0600

    finished header testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,24 +1,41 @@
 import React from "react";
-import { render, screen, fireEvent  } from "@testing-library/react";
-import App from '../App';
+import { render, screen,  } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import App from '../App';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {
+test("form page header renders", () => {
     //Arrange
-    render (<App />)
+    render (<App />);
     //Act
     const formHeader = screen.getByText(/react plants/i);
+    const headerImg = screen.getByRole('img');
     //Assert
     expect(formHeader).toBeInTheDocument();
+    expect(headerImg).toBeInTheDocument();
 });
+
+test('form header render', ()=>{
+    //Arrange
+    render (<CheckoutForm />);
+    
+    //Act
+    const checkoutFormHeader = screen.getByText(/checkout form/i);
+
+    //Assert
+    expect(checkoutFormHeader).toBeInTheDocument;
+    expect(checkoutFormHeader).toBeTruthy;
+    
+})
 
 test("form shows success message on submit with form details", () => {
     //Arrange
-    // render (<CheckoutForm />)
+     render (<CheckoutForm />);
 
     //Act
+
+
     const onSubmit = jest.fn();
     const { getByTestId } = render(<CheckoutForm onSubmit= {onSubmit}/>);
     
